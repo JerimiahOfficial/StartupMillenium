2ea68df5254f493ac058042afd52ce8507530d5d
Jerimiah Official <38990407+JerimiahOfficial@users.noreply.github.com>
2019-07-08 03:16:20 -0400

    Add files via upload

---
+++ OpenGarrysMod1000Times/OpenGarrysMod1000Times/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace OpenGarrysMod1000Times
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.start_Button = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.youtube_Button = new System.Windows.Forms.Button();
+            this.github_Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Startup Millenium";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(95, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(393, 75);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Start Garry\'s Mod 1000 times";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.github_Button);
+            this.panel2.Controls.Add(this.youtube_Button);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.start_Button);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(500, 175);
+            this.panel2.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // start_Button
+            // 
+            this.start_Button.Appearance = System.Windows.Forms.Appearance.Button;
+            this.start_Button.AutoSize = true;
+            this.start_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.start_Button.FlatAppearance.BorderSize = 0;
+            this.start_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.start_Button.Location = new System.Drawing.Point(441, 135);
+            this.start_Button.Name = "start_Button";
+            this.start_Button.Size = new System.Drawing.Size(47, 28);
+            this.start_Button.TabIndex = 0;
+            this.start_Button.Text = "Start";
+            this.start_Button.UseVisualStyleBackColor = false;
+            this.start_Button.CheckedChanged += new System.EventHandler(this.start_Button_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Webdings", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(476, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "r";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Made by: Jerimiah";
+            // 
+            // youtube_Button
+            // 
+            this.youtube_Button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("youtube_Button.BackgroundImage")));
+            this.youtube_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.youtube_Button.FlatAppearance.BorderSize = 0;
+            this.youtube_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.youtube_Button.Location = new System.Drawing.Point(48, 133);
+            this.youtube_Button.Name = "youtube_Button";
+            this.youtube_Button.Size = new System.Drawing.Size(30, 30);
+            this.youtube_Button.TabIndex = 4;
+            this.youtube_Button.UseVisualStyleBackColor = true;
+            this.youtube_Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // github_Button
+            // 
+            this.github_Button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("github_Button.BackgroundImage")));
+            this.github_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.github_Button.FlatAppearance.BorderSize = 0;
+            this.github_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.github_Button.Location = new System.Drawing.Point(12, 133);
+            this.github_Button.Name = "github_Button";
+            this.github_Button.Size = new System.Drawing.Size(30, 30);
+            this.github_Button.TabIndex = 5;
+            this.github_Button.UseVisualStyleBackColor = true;
+            this.github_Button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(500, 175);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckBox start_Button;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button github_Button;
+        private System.Windows.Forms.Button youtube_Button;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ OpenGarrysMod1000Times/OpenGarrysMod1000Times/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace OpenGarrysMod1000Times
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Button_CheckedChanged(object sender, EventArgs e)
+        {
+            if (start_Button.Checked)
+            {
+                timer1.Start();
+                start_Button.Text = "Stop";
+                start_Button.BackColor = Color.FromArgb(255, 0, 0);
+            }
+            else
+            {
+                timer1.Stop();
+                start_Button.Text = "Start";
+                start_Button.BackColor = Color.FromArgb(0, 127, 0);
+                foreach (Process proc in Process.GetProcessesByName("hl2"))
+                {
+                    proc.Kill();
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int procKilled = 1;
+            foreach (Process proc in Process.GetProcessesByName("hl2"))
+            {
+                proc.Kill();
+                procKilled = 1;
+            }
+
+            if (procKilled == 1)
+            {
+                const string fileDir = "F:\\Program Files (x86)\\Steam\\steamapps\\common\\GarrysMod\\hl2.exe";
+                Process.Start(fileDir);
+                procKilled = 0;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/JerimiahOfficial");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.youtube.com/channel/UCHi47xiIYDponERT6IXLRew");
+        }
+    }
+}
