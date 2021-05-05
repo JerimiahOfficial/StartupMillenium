eeae1cdcc6d0d4fc6d9d80c32029da03e96784e1
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2021-05-04 23:49:44 -0400

    Fixing

---
+++ OpenGarrysMod1000Times/OpenGarrysMod1000Times/SM.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Startup_Millenium
+{
+    partial class SM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SM));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.content = new System.Windows.Forms.Panel();
+            this.dirButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.Checker = new System.Windows.Forms.Timer(this.components);
+            this.Status = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.content.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Startup Millenium";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(83, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(304, 75);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(99)))), ((int)(((byte)(98)))));
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Start Garry\'s Mod 1000 times";
+            // 
+            // content
+            // 
+            this.content.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.content.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.content.Controls.Add(this.dirButton);
+            this.content.Controls.Add(this.closeButton);
+            this.content.Controls.Add(this.panel1);
+            this.content.Controls.Add(this.startButton);
+            this.content.Controls.Add(this.pictureBox1);
+            this.content.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.content.Location = new System.Drawing.Point(0, 0);
+            this.content.Name = "content";
+            this.content.Padding = new System.Windows.Forms.Padding(10);
+            this.content.Size = new System.Drawing.Size(400, 265);
+            this.content.TabIndex = 4;
+            // 
+            // dirButton
+            // 
+            this.dirButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.dirButton.FlatAppearance.BorderSize = 0;
+            this.dirButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dirButton.Font = new System.Drawing.Font("Roboto", 12F);
+            this.dirButton.Location = new System.Drawing.Point(10, 91);
+            this.dirButton.Name = "dirButton";
+            this.dirButton.Size = new System.Drawing.Size(377, 50);
+            this.dirButton.TabIndex = 8;
+            this.dirButton.TabStop = false;
+            this.dirButton.Text = "Garrysmod Directory";
+            this.dirButton.UseVisualStyleBackColor = false;
+            this.dirButton.Click += new System.EventHandler(this.dirButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Font = new System.Drawing.Font("Roboto", 12F);
+            this.closeButton.Location = new System.Drawing.Point(10, 203);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(377, 50);
+            this.closeButton.TabIndex = 7;
+            this.closeButton.TabStop = false;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Green;
+            this.startButton.FlatAppearance.BorderSize = 0;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ImageIndex = 0;
+            this.startButton.Location = new System.Drawing.Point(10, 147);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(377, 50);
+            this.startButton.TabIndex = 6;
+            this.startButton.TabStop = false;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Checker
+            // 
+            this.Checker.Tick += new System.EventHandler(this.Checker_Tick);
+            // 
+            // Status
+            // 
+            this.Status.Enabled = true;
+            this.Status.Tick += new System.EventHandler(this.Status_Tick);
+            // 
+            // SM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(400, 265);
+            this.Controls.Add(this.content);
+            this.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "SM";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Startup Millenium";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SM_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.content.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel content;
+        private System.Windows.Forms.Timer Checker;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Timer Status;
+        private System.Windows.Forms.Button dirButton;
+    }
+}
+
---
+++ OpenGarrysMod1000Times/OpenGarrysMod1000Times/SM.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Startup_Millenium
+{
+    public partial class SM : Form
+    {
+        public static bool      curStatus   = false;
+        public static bool      procKilled  = false;
+        public static string    dir         = "";
+
+        public SM() {
+            InitializeComponent();
+        }
+
+        private void dirButton_Click(object sender, EventArgs e) {
+            // Create OFD.
+            OpenFileDialog gmoddir = new OpenFileDialog();
+
+            // Show OFD.
+            gmoddir.ShowDialog();
+
+            // Set directory to the file name.
+            dir = gmoddir.FileName;
+        }
+
+        private void startButton_Click(object sender, EventArgs e) {
+            if (dir.Length <= 0) {
+                MessageBox.Show("You must use the garrysmod directory button to set a directory.", "Missing directory!", MessageBoxButtons.OK);
+                return;
+            }
+
+            // Change status inside switch.
+            curStatus = !curStatus;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e) {
+            // Exit application when clicked.
+            Application.Exit();
+        }
+
+        private void Status_Tick(object sender, EventArgs e) {
+            switch (curStatus) {
+                case false: // If program isn't running, set button back to Start.
+                    startButton.Text = "Start";
+                    startButton.BackColor = Color.Green;
+                    Checker.Stop();
+                    break;
+
+                case true: // If program is running, set button to Stop.
+                    startButton.Text = "Stop";
+                    startButton.BackColor = Color.Red;
+                    Checker.Start();
+                    break;
+            }
+        }
+
+        private void Checker_Tick(object sender, EventArgs e) {
+            // If the process was killed then start garrysmod.
+            if (!procKilled) {
+                Process.Start(dir);
+                procKilled = !procKilled;
+            }
+
+            // Close hl2.exe everytime there is and instance.
+            if (procKilled) {
+                try {
+                    foreach (Process proc in Process.GetProcessesByName("hl2")) {
+                        proc.Kill();
+                    }
+                } catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void SM_FormClosed(object sender, FormClosedEventArgs e) {
+            // Try to close all processes named hl2 when form closed.
+            try {
+                foreach (Process proc in Process.GetProcessesByName("hl2")) {
+                    proc.Kill();
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
