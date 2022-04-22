0e8c559b38e76d6e19c6f4aceab4dce592202125
Jerimiah <JerimiahOfficial@gmail.com>
2022-04-22 11:41:26 -0400

    Added git ignore

--- OpenGarrysMod1000Times/Startup-Millenium/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ OpenGarrysMod1000Times/Startup-Millenium/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -62,7 +62,7 @@ namespace Startup_Millenium {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Startup-Millenium;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Startup-Millenium;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
