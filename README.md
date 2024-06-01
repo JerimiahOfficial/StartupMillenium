5265b19d9ddcab4a342e6f401af731061bc7c692
Jerimiah <gage.lodba@proton.me>
2024-06-01 01:57:40 -0400

    Rewritten in C++

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
-# Startup-Millenium
-This project was made so I could get an achievement in a game called garrysmod. Startup-Millenium opens and closes you garrysmod at specific times in order for steam to register the game as open. It is capable of searching drives if the game isn't found on the `C:\` drive and comes with an expected wait time of around 25 - 35 seconds depending on computer hardware.
+### StartupMillenium
+A simple program to get the acheivement for a game called Garry's Mod. This acheivement requires you to open and close the game 1000 times.
 
-[Showcase Video (older version)](https://www.youtube.com/watch?v=1sbe09a9vJo)
+> [!NOTE]
+> The program is hardcoded to `C:\Program Files (x86)\Steam\steamapps\common\GarrysMod`. If you have the game on another hard drive it will not work unless you change the path.
 
-# User Interface
-![image](https://user-images.githubusercontent.com/38990407/164753359-ff2c8931-6e84-49e8-9279-99d14165215e.png)
+### Tested platforms
+It has been tested and verified working on windows. So far the only supported platform is windows. I am planning on adding in linux support in the future.
+
+### Dependencies:
+[CMake](https://cmake.org/) is an open-source, cross-platform family of tools designed to build, test and package software.
+
+#### Optional
+gcc and g++ for windows:  
+[msys2](https://www.msys2.org/) is a collection of tools and libraries providing you with an easy-to-use environment for building, installing and running native Windows software.
+
+### Building instructions
+
+#### Windows
+`cmake -B build -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc`  
+`cmake --build build --config Release`
+
+<!-- 
+Will be added in when linux support is added.
+#### Linux
+
+`cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc`  
+`cmake --build build --config Release`
+-->
+
---
+++ source/main.cpp
@@ -0,0 +1,94 @@
+#include <filesystem>
+#include <iostream>
+#include <thread>
+
+#ifdef _WIN32
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+
+std::string path =
+    "C:\\Program Files (x86)\\Steam\\steamapps\\common\\GarrysMod";
+
+_PROCESS_INFORMATION startup(LPCSTR lpApplicationName) {
+  bool bSuccess = false;
+
+  _STARTUPINFOA si;
+  _PROCESS_INFORMATION pi;
+
+  ZeroMemory(&si, sizeof(si));
+  si.cb = sizeof(si);
+  ZeroMemory(&pi, sizeof(pi));
+
+  bSuccess = CreateProcessA(lpApplicationName, NULL, NULL, NULL, TRUE, 0, NULL,
+                            NULL, &si, &pi);
+
+  if (!bSuccess) {
+    std::cout << "Failed to create process.\n";
+  }
+
+  return pi;
+}
+
+#elif __linux__
+// TODO: Implement Linux
+#endif
+
+int main() {
+#ifdef __linux__
+  // TODO: Implement Linux support
+  std::cout << "This program is not supported on Linux yet.\n";
+  std::cout << "Press enter to exit...\n";
+  std::cin.get();
+  return 0;
+#endif
+
+  std::cout << "Welcome to Startup Millenium\n";
+
+  if (!std::filesystem::exists(path)) {
+    std::cout << "GarrysMod directory not found.\n";
+    std::cout << "Press enter to exit...\n";
+    std::cin.get();
+    return 0;
+  }
+
+  std::cout << "GarrysMod directory found.\n";
+
+  std::filesystem::current_path(path);
+
+  std::cout << "Starting loop...\n";
+
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+
+  for (int i = 1; i < 1000; i++) {
+#ifdef _WIN32
+    PROCESS_INFORMATION pInfo = startup("hl2.exe");
+
+    if (pInfo.hProcess == NULL) {
+      std::cout << "Failed to start Garrysmod.\n";
+      return 1;
+    }
+#elif __linux__
+    // TODO: Implement Linux support
+#endif
+
+    std::this_thread::sleep_for(std::chrono::seconds(15));
+
+#ifdef _WIN32
+    TerminateProcess(pInfo.hProcess, 0);
+    CloseHandle(pInfo.hProcess);
+    CloseHandle(pInfo.hThread);
+#elif __linux__
+    // TODO: Implement Linux support
+#endif
+
+    std::cout << i << " / 1000\n";
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+  }
+
+  std::cout << "Loop finished.\n";
+  std::cout << "Press enter to exit...\n";
+  std::cin.get();
+
+  return 0;
+}
